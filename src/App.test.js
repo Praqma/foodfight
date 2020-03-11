1cb33409863217f5aab1fc8cf35ef96f9fd564d9
Ester Daniel Ytterbrink <esterdaniel.ytterbrink@eficode.com>
2020-03-11 09:44:24 +0100

    Make it about food

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1 = getByText(/My food/i);
+  expect(h1).toBeInTheDocument();
 });
